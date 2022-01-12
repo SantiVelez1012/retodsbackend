dbc85f33673a2e0d4b1768b5adc303b2190845c8
SantiVelez1012 <63984260+SantiVelez1012@users.noreply.github.com>
2022-01-12 11:39:08 -0500

    creando repositorios y consultas para busqueda de casas

--- src/main/java/com/rdai/data/repository/CasaRepository.java
+++ src/main/java/com/rdai/data/repository/CasaRepository.java
@@ -2,13 +2,14 @@ package com.rdai.data.repository;
 
 
 import com.rdai.data.models.entities.CasaEntity;
+import com.rdai.data.repository.custom.CriterioCasaRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CasaRepository extends JpaRepository<CasaEntity, Integer> {
+public interface CasaRepository extends JpaRepository<CasaEntity, Integer>, CriterioCasaRepository {
 
     List<CasaEntity> findAllByPais(String pais);
 
